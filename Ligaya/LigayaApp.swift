fd98ac08e3b6222da6dd3e51a2173e8770aa0ed9
chicodevbr <chicodevbr@gmail.com>
2022-10-10 23:02:27 -0300

    Initial Commit

---
+++ Ligaya/LigayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LigayaApp.swift
+//  Ligaya
+//
+//  Created by fjunior on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LigayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
